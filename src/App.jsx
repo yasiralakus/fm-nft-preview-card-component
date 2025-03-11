52f9663971d923eb33ae04be1c577967468295d4
yasiralakus <yasiralakus11@gmail.com>
2025-03-11 18:20:59 +0300

    project completed

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+export default function App() {
+
+    return (
+        <div className="full-page">
+
+            <div className="main-box">
+
+                <div className="img-box">
+                    <img src="./images/image-equilibrium.jpg" alt="" />
+                    <div className="hover">
+                        <img src="./images/icon-view.svg" alt="" />
+                    </div>
+                </div>
+
+                <h1>Equilibrium #3429</h1>
+
+                <p>Our Equilibrium collection promotes balance and calm.</p>
+
+                <div className="value">
+                    <div className="eth">
+                        <img src="./images/icon-ethereum.svg" alt="" />
+                        <h1>0.041 ETH</h1>
+                    </div>
+                    <div className="days">
+                        <img src="./images/icon-clock.svg" alt="" />
+                        <h1>3 days left</h1>
+                    </div>
+                </div>
+
+                <div className="creating">
+                    <img src="./images/image-avatar.png" alt="" />
+                    <p>Creation of <span>Jules Wyvern</span></p>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
